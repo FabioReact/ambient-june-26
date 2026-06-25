348009957f939ac7fe205c4a06ae8d7f421e1cb6
Fabio Domingues <fabio.react.dev@gmail.com>
2026-06-25 09:04:52 +0200

    style: fix navigation ui

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -21,52 +21,52 @@ const MainLayout = () => {
     <>
       <header className='sticky top-0 z-20 border-b border-border/70 bg-background/80 backdrop-blur-xl'>
         <nav className='mx-auto flex min-h-18 max-w-7xl items-center justify-between gap-4 px-4 sm:px-6 lg:px-8'>
-          <NavLink
-            className={(state) =>
-              `rounded-lg px-3 py-2 text-sm font-medium transition-colors ${getActiveClassNames(state)}`
-            }
-            to='/'
-          >
-            Home
+          <NavLink to='/' className='flex shrink-0 items-center gap-2 font-semibold tracking-tight'>
+            <span className='grid size-8 place-items-center rounded-lg bg-primary text-sm font-black text-primary-foreground'>
+              H
+            </span>
+            <span>Hero Nexus</span>
           </NavLink>
-          <NavLink className={getActiveClassNames} to='/heroes'>
-            Heroes
-          </NavLink>
-          <NavLink className={getActiveClassNames} to='/squad'>
-            Squad
-          </NavLink>
-          <NavLink className={getActiveClassNames} to='/battle'>
-            Battle
-          </NavLink>
-          <DropdownMenu>
-            <DropdownMenuTrigger
-              render={
-                <Button variant='ghost' className='cursor-pointer'>
-                  Learning
-                </Button>
-              }
-            />
-            <DropdownMenuContent>
-              <DropdownMenuGroup>
-                <DropdownMenuItem>
-                  <NavLink to='/learning/useState'>useState</NavLink>
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                  <NavLink to='/learning/useEffect'>useEffect</NavLink>
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                  <NavLink to='/learning/useRef'>useRef</NavLink>
-                </DropdownMenuItem>
-              </DropdownMenuGroup>
-            </DropdownMenuContent>
-          </DropdownMenu>
+          <div className='flex flex-wrap justify-end gap-1'>
+            <NavLink className={getActiveClassNames} to='/heroes'>
+              Heroes
+            </NavLink>
+            <NavLink className={getActiveClassNames} to='/squad'>
+              Squad
+            </NavLink>
+            <NavLink className={getActiveClassNames} to='/battle'>
+              Battle
+            </NavLink>
+            <DropdownMenu>
+              <DropdownMenuTrigger
+                render={
+                  <Button variant='ghost' className='cursor-pointer'>
+                    Learning
+                  </Button>
+                }
+              />
+              <DropdownMenuContent>
+                <DropdownMenuGroup>
+                  <DropdownMenuItem>
+                    <NavLink to='/learning/useState'>useState</NavLink>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <NavLink to='/learning/useEffect'>useEffect</NavLink>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <NavLink to='/learning/useRef'>useRef</NavLink>
+                  </DropdownMenuItem>
+                </DropdownMenuGroup>
+              </DropdownMenuContent>
+            </DropdownMenu>
 
-          <NavLink className={getActiveClassNames} to='/login'>
-            Login
-          </NavLink>
-          <NavLink className={getActiveClassNames} to='/register'>
-            Register
-          </NavLink>
+            <NavLink className={getActiveClassNames} to='/login'>
+              Login
+            </NavLink>
+            <NavLink className={getActiveClassNames} to='/register'>
+              Register
+            </NavLink>
+          </div>
         </nav>
       </header>
       <main className='mx-auto min-h-[calc(100vh-4.5rem)] w-full max-w-7xl px-4 py-8 sm:px-6 lg:px-8'>
